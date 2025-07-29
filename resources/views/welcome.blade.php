763e2b492b476446e87601463e9e5f3d5b31bd14
lukashirikato <farhanrizkysaputra2508@gmail.com>
2025-07-29 18:56:52 +0700

    Fix favicon path

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- favicon  -->
 <!-- filepath: resources/views/dashboard.blade.php -->
-<link rel="icon" type="image/png" href="{{ asset('images/favicon.jpg') }}" />
+<link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}" />
     <!-- end favicon  -->
     <title>FTM SOCIETY - Muslimah-Only Gym</title>
     <script src="https://cdn.tailwindcss.com/3.4.16"></script>
